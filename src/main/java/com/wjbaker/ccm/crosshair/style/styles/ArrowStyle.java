acc9614effee471f07b37da05f6d6d568c190840
wjbaker101 <willbaker10198@gmail.com>
2021-06-20 12:56:10 +0100

    Remove outline for the arrow style. With the line being so thin, it was difficult to see it

--- src/main/java/com/wjbaker/ccm/crosshair/style/styles/ArrowStyle.java
+++ src/main/java/com/wjbaker/ccm/crosshair/style/styles/ArrowStyle.java
@@ -3,7 +3,6 @@ package com.wjbaker.ccm.crosshair.style.styles;
 import com.wjbaker.ccm.crosshair.CustomCrosshair;
 import com.wjbaker.ccm.crosshair.render.ComputedProperties;
 import com.wjbaker.ccm.crosshair.style.AbstractCrosshairStyle;
-import com.wjbaker.ccm.type.RGBA;
 import net.minecraft.client.util.math.MatrixStack;
 
 public final class ArrowStyle extends AbstractCrosshairStyle {
@@ -14,21 +13,9 @@ public final class ArrowStyle extends AbstractCrosshairStyle {
 
     @Override
     public void draw(final int x, final int y, final ComputedProperties computedProperties) {
-        boolean isOutlineEnabled = this.crosshair.isOutlineEnabled.get();
         int width = this.crosshair.width.get();
         int height = this.crosshair.height.get();
         int thickness = this.crosshair.thickness.get();
-
-        if (isOutlineEnabled) {
-            RGBA outlineColour = this.crosshair.outlineColour.get();
-
-            this.renderManager.drawLines(this.matrixStack, new float[] {
-                x - width - 1, y + height + 1, x, y,
-                x, y, x + width + 1, y + height + 1
-            }, thickness + 3, outlineColour);
-
-            this.renderManager.drawCircle(this.matrixStack, x, y, thickness == 1 ? 0.5F : thickness / 5.0F, 1.0F, outlineColour);
-        }
 
         this.renderManager.drawLines(this.matrixStack, new float[] {
             x - width, y + height, x, y,
