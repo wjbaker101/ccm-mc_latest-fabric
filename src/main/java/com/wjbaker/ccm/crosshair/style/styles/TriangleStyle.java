5506469316c31a92dde752de7565c18fc2421ff8
wjbaker101 <willbaker10198@gmail.com>
2021-12-18 12:04:57 +0000

    Remove outline for triangle style, it looks ugly since it cannot accurately outline the lines

--- src/main/java/com/wjbaker/ccm/crosshair/style/styles/TriangleStyle.java
+++ src/main/java/com/wjbaker/ccm/crosshair/style/styles/TriangleStyle.java
@@ -13,36 +13,11 @@ public final class TriangleStyle extends AbstractCrosshairStyle {
 
     @Override
     public void draw(final int x, final int y, final ComputedProperties computedProperties) {
-        var isOutlineEnabled = this.crosshair.isOutlineEnabled.get();
         var width = this.crosshair.width.get();
         var height = this.crosshair.height.get();
         var gap = computedProperties.gap();
         var colour = computedProperties.colour();
         var isAdaptiveColourEnabled = this.crosshair.isAdaptiveColourEnabled.get();
-
-        if (isOutlineEnabled) {
-            var outlineColour = this.crosshair.outlineColour.get();
-
-            // Outer
-            this.renderManager.drawLines(this.matrixStack, new float[] {
-                x, y - (height / 2.0F) - gap - 0.5F,
-                x + width / 2.0F + gap + 1.0F, y + (height / 2.0F) + gap + 0.5F,
-                x + width / 2.0F + gap + 1.0F, y + (height / 2.0F) + gap + 0.5F,
-                x - (width / 2.0F) - gap - 1.0F, y + (height / 2.0F) + gap + 0.5F,
-                x - (width / 2.0F) - gap - 1.0F, y + (height / 2.0F) + gap + 0.5F,
-                x, y - (height / 2.0F) - gap - 1.0F
-            }, 2.0F, outlineColour);
-
-            // Inner
-            this.renderManager.drawLines(this.matrixStack, new float[] {
-                x, y - (height / 2.0F) - gap + 0.5F,
-                x + width / 2.0F + gap - 1.0F, y + (height / 2.0F) + gap - 0.5F,
-                x + width / 2.0F + gap - 1.0F, y + (height / 2.0F) + gap - 0.5F,
-                x - (width / 2.0F) - gap + 1.0F, y + (height / 2.0F) + gap - 0.5F,
-                x - (width / 2.0F) - gap + 1.0F, y + (height / 2.0F) + gap - 0.5F,
-                x, y - (height / 2.0F) - gap + 1.0F
-            }, 2.0F, outlineColour);
-        }
 
         this.renderManager.drawLines(this.matrixStack, new float[] {
             x, y - (height / 2.0F) - gap,
