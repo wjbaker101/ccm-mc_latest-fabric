314b58182afa81a8ee37ff9c9478f8fb644090ec
wjbaker101 <willbaker10198@gmail.com>
2022-07-02 18:14:50 +0100

    Bump version

--- src/main/java/com/wjbaker/ccm/CustomCrosshairMod.java
+++ src/main/java/com/wjbaker/ccm/CustomCrosshairMod.java
@@ -22,7 +22,7 @@ public final class CustomCrosshairMod implements ModInitializer {
     public static CustomCrosshairMod INSTANCE;
 
     public static final String TITLE = "Custom Crosshair Mod";
-    public static final String VERSION = "1.4.0-fabric";
+    public static final String VERSION = "1.5.0-fabric";
     public static final String MC_VERSION = "1.19-fabric";
     public static final String CURSEFORGE_PAGE = "https://www.curseforge.com/projects/242995/";
     public static final String MC_FORUMS_PAGE = "https://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/2637819/";
