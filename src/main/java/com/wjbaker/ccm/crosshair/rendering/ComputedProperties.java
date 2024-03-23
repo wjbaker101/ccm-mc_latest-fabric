a148177debab3e72c235745958d0418b6c58cb8c
William Baker <wjbaker101@users.noreply.github.com>
2024-03-23 19:09:44 +0000

    Move specific compute logic in own class

--- src/main/java/com/wjbaker/ccm/crosshair/rendering/ComputedProperties.java
+++ src/main/java/com/wjbaker/ccm/crosshair/rendering/ComputedProperties.java
@@ -2,16 +2,13 @@ package com.wjbaker.ccm.crosshair.rendering;
 
 import com.google.common.collect.ImmutableSet;
 import com.wjbaker.ccm.crosshair.CustomCrosshair;
+import com.wjbaker.ccm.crosshair.rendering.computed.ComputeColour;
 import com.wjbaker.ccm.crosshair.rendering.computed.ComputeGap;
 import com.wjbaker.ccm.crosshair.rendering.computed.ComputeIndicators;
 import com.wjbaker.ccm.type.RGBA;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.Perspective;
-import net.minecraft.entity.mob.Angerable;
-import net.minecraft.entity.mob.Monster;
-import net.minecraft.entity.passive.PassiveEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 
@@ -43,7 +40,7 @@ public final class ComputedProperties {
         this.crosshair = crosshair;
 
         this.gap = ComputeGap.compute(crosshair);
-        this.colour = this.calculateColour();
+        this.colour = ComputeColour.compute(crosshair);
         this.isVisible = this.calculateIsVisible();
     }
 
@@ -57,48 +54,6 @@ public final class ComputedProperties {
 
     public boolean isVisible() {
         return this.isVisible;
-    }
-
-    private RGBA calculateColour() {
-        var target = this.mc.targetedEntity;
-
-        var isHighlightPlayersEnabled = this.crosshair.isHighlightPlayersEnabled.get();
-        if (isHighlightPlayersEnabled && target instanceof PlayerEntity)
-            return this.crosshair.highlightPlayersColour.get();
-
-        var isHighlightHostilesEnabled = this.crosshair.isHighlightHostilesEnabled.get();
-        if (isHighlightHostilesEnabled && (target instanceof Monster || target instanceof Angerable))
-            return this.crosshair.highlightHostilesColour.get();
-
-        var isHighlightPassivesEnabled = this.crosshair.isHighlightPassivesEnabled.get();
-        if (isHighlightPassivesEnabled && target instanceof PassiveEntity)
-            return this.crosshair.highlightPassivesColour.get();
-
-        if (this.crosshair.isRainbowEnabled.get())
-            return this.getRainbowColour();
-
-        return this.crosshair.colour.get();
-    }
-
-    private RGBA getRainbowColour() {
-        var ticks = this.crosshair.rainbowTicks.get() + 1;
-
-        if (ticks > 125000)
-            ticks = 0;
-
-        this.crosshair.rainbowTicks.set(ticks);
-
-        var opacity = this.crosshair.colour.get().getOpacity();
-        var speed = this.crosshair.rainbowSpeed.get();
-
-        return new RGBA(255, 255, 255, opacity)
-            .setRed(this.getRainbowColourComponent(ticks, 0.0F, speed))
-            .setGreen(this.getRainbowColourComponent(ticks, 2.0F, speed))
-            .setBlue(this.getRainbowColourComponent(ticks, 4.0F, speed));
-    }
-
-    private int getRainbowColourComponent(final int ticks, final float offset, final int speed) {
-        return (int)(Math.sin((ticks * speed / 20000.0F) + offset) * 127 + 128);
     }
 
     private boolean calculateIsVisible() {
---
+++ src/main/java/com/wjbaker/ccm/crosshair/rendering/computed/ComputeColour.java
@@ -0,0 +1,56 @@
+package com.wjbaker.ccm.crosshair.rendering.computed;
+
+import com.wjbaker.ccm.crosshair.CustomCrosshair;
+import com.wjbaker.ccm.type.RGBA;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.mob.Angerable;
+import net.minecraft.entity.mob.Monster;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.player.PlayerEntity;
+
+public final class ComputeColour {
+
+    private static final MinecraftClient mc = MinecraftClient.getInstance();
+
+    public static RGBA compute(final CustomCrosshair crosshair) {
+        var target = mc.targetedEntity;
+
+        var isHighlightPlayersEnabled = crosshair.isHighlightPlayersEnabled.get();
+        if (isHighlightPlayersEnabled && target instanceof PlayerEntity)
+            return crosshair.highlightPlayersColour.get();
+
+        var isHighlightHostilesEnabled = crosshair.isHighlightHostilesEnabled.get();
+        if (isHighlightHostilesEnabled && (target instanceof Monster || target instanceof Angerable))
+            return crosshair.highlightHostilesColour.get();
+
+        var isHighlightPassivesEnabled = crosshair.isHighlightPassivesEnabled.get();
+        if (isHighlightPassivesEnabled && target instanceof PassiveEntity)
+            return crosshair.highlightPassivesColour.get();
+
+        if (crosshair.isRainbowEnabled.get())
+            return getRainbowColour(crosshair);
+
+        return crosshair.colour.get();
+    }
+
+    private static RGBA getRainbowColour(final CustomCrosshair crosshair) {
+        var ticks = crosshair.rainbowTicks.get() + 1;
+
+        if (ticks > 125000)
+            ticks = 0;
+
+        crosshair.rainbowTicks.set(ticks);
+
+        var opacity = crosshair.colour.get().getOpacity();
+        var speed = crosshair.rainbowSpeed.get();
+
+        return new RGBA(255, 255, 255, opacity)
+            .setRed(getRainbowColourComponent(ticks, 0.0F, speed))
+            .setGreen(getRainbowColourComponent(ticks, 2.0F, speed))
+            .setBlue(getRainbowColourComponent(ticks, 4.0F, speed));
+    }
+
+    private static int getRainbowColourComponent(final int ticks, final float offset, final int speed) {
+        return (int)(Math.sin((ticks * speed / 20000.0F) + offset) * 127 + 128);
+    }
+}
